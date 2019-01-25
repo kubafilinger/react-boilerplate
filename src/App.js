953854b2d7b488b18f67c342220c45b309a63099
Jakub Filinger <jakub.filinger@exchange-technologies.com>
2019-01-25 18:06:19 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Kanban from './Kanban'
+
+ReactDOM.render(<Kanban/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./src/App.js",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    output: {
+        publicPath: "/dist/",
+        path: __dirname + "/dist/",
+        filename: "app.min.js"
+    }
+};
